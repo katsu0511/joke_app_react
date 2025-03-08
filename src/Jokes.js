3292ed04e4b4618a92e77a64821a5ac564942d3d
Katsuya <haradakatsuya190511@gmail.com>
2025-03-07 22:03:07 -0800

    added putPunchline

--- src/Jokes.js
+++ src/Jokes.js
@@ -11,14 +11,19 @@ export default function Wanted() {
   const [punchline, setPunchline] = useState("");
   const { data } = useQuery('jokes', fetchJokes);
 
+  function putPunchline() {
+    const answer = document.getElementById('answer');
+    setPunchline(answer.value);
+  }
+
   return (
     <section className='jokes'>
       <div className='joke'>
         <p>Type: {data.type}</p>
         <p>Setup: {data.setup}</p>
         <p>What is the punchline?</p>
-        <input type='text' />
-        <input type='button' value='push' />
+        <input type='text' id='answer' />
+        <input type='button' value='push' onClick={putPunchline} />
         <p>Punchline: {data.punchline}</p>
       </div>
     </section>

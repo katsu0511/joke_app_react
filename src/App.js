aac993fade20801e2a3db5435eaaf8caa568c978
Katsuya <haradakatsuya190511@gmail.com>
2025-03-19 22:46:08 -0700

    add fix

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       <header>
         <h1>Jokes</h1>
       </header>
-      <Suspense fallback={<p>Loadin</p>}>
+      <Suspense fallback={<p>Load</p>}>
         <ErrorBoundary fallback={<p>Error has happened</p>}>
           <QueryClientProvider client={cli}>
             <Jokes />
